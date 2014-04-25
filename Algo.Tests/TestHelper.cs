dfded9204b780168a3993028e7a051b1043dbf3c
Olivier Spinelli <olivier.spinelli@invenietis.com>
2014-04-25 08:36:45 +0200

    First commit.

---
+++ Algo.Tests/TestHelper.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Algo.Tests
+{
+    [ExcludeFromCodeCoverage]
+    public static partial class TestHelper
+    {
+
+        static string _testFolder;
+        static string _solutionFolder;
+
+        static TestHelper()
+        {
+        }
+
+        public static string TestFolder
+        {
+            get
+            {
+                if( _testFolder == null ) InitalizePaths();
+                return _testFolder;
+            }
+        }
+
+        public static string SolutionFolder
+        {
+            get
+            {
+                if( _solutionFolder == null ) InitalizePaths();
+                return _solutionFolder;
+            }
+        }
+
+        public static void CleanupTestFolder()
+        {
+            DeleteFolder( TestFolder, true );
+        }
+
+        public static void DeleteFolder( string directoryPath, bool recreate = false )
+        {
+            int tryCount = 0;
+            for( ; ; )
+            {
+                try
+                {
+                    if( Directory.Exists( directoryPath ) ) Directory.Delete( directoryPath, true );
+                    if( recreate )
+                    {
+                        Directory.CreateDirectory( directoryPath );
+                        File.WriteAllText( Path.Combine( directoryPath, "TestWrite.txt" ), "Test write works." );
+                        File.Delete( Path.Combine( directoryPath, "TestWrite.txt" ) );
+                    }
+                    return;
+                }
+                catch( Exception )
+                {
+                    if( ++tryCount == 20 ) throw;
+                    System.Threading.Thread.Sleep( 100 );
+                }
+            }
+        }
+
+        private static void InitalizePaths()
+        {
+            string p = new Uri( System.Reflection.Assembly.GetExecutingAssembly().CodeBase ).LocalPath;
+            // => CK.XXX.Tests/bin/Debug/
+            p = Path.GetDirectoryName( p );
+            // => CK.XXX.Tests/bin/
+            p = Path.GetDirectoryName( p );
+            // => CK.XXX.Tests/
+            p = Path.GetDirectoryName( p );
+            // ==> CK.XXX.Tests/TestDir
+            _testFolder = Path.Combine( p, "TestFolder" );
+            do
+            {
+                p = Path.GetDirectoryName( p );
+            }
+            while( !File.Exists( Path.Combine( p, "Algo.sln" ) ) );
+            _solutionFolder = p;
+
+            CleanupTestFolder();
+        }
+
+    }
+}
